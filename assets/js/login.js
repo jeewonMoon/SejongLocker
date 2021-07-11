5d27eee2ecc3a1a94b9470eaf6a1ab8214dd8afb
jeewonMoon <papers1210@kakao.com>
2021-07-11 13:19:25 +0900

    C

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,12 @@
 function CheckEmail(){
-    let email = document.getElementsByClassName('email').value;
-    let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g])*.([a][c])*.([k][r])$/
-    
-    if (!regExp.test(email)){
+    let address = document.getElementsByClassName("email1").value;
+    //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
+    let regExp = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+    if (!regExp.test(address)){
         alert("이메일을 확인해주세요.");
-        //document.getElementsByClassName('email').innerHTML("올바른 이메일을 입력해주세요.");
+        //document.getElementsByClassName('email1').innerHTML("올바른 이메일을 입력해주세요.");
     }else{
-        
+        alert("correct");
     }
 }
