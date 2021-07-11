900f509cc94f89f84aa821e8e43c03439872e4e4
jeewonMoon <papers1210@kakao.com>
2021-07-11 21:11:40 +0900

    ㅁ

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,5 @@
 function CheckUserEmail(){
-    let email = document.getElementsByClassName("user-email").value;
+    let email = document.getElementsByClassName('user-email').value;
     //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
     let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
@@ -12,7 +12,7 @@ function CheckUserEmail(){
 }
 
 function CheckAdminEmail(){
-    let email = document.getElementsByClassName("admin-email").value;
+    let email = document.getElementsByClassName('admin-email').value;
     //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
     let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
