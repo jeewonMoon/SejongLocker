c0e058516c32b0ae0f8131e31bc3cc7ea683cf90
jeewonMoon <papers1210@kakao.com>
2021-07-11 19:30:52 +0900

    헤더 수정

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,22 @@
-function CheckEmail(){
+function CheckUserEmail(){
     let email = document.getElementsByClassName("user-email").value;
     //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
-    let reg = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
-    if (!reg.test(email)){
+    if (!regex.test(email)){
+        alert("이메일을 확인해주세요.");
+        //document.getElementsByClassName("user-email").innerHTML("올바른 이메일을 입력해주세요.");
+    }else{
+        alert("correct");
+    }
+}
+
+function CheckAdminEmail(){
+    let email = document.getElementsByClassName("admin-email").value;
+    //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
+    let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+    if (!regex.test(email)){
         alert("이메일을 확인해주세요.");
         //document.getElementsByClassName("user-email").innerHTML("올바른 이메일을 입력해주세요.");
     }else{
