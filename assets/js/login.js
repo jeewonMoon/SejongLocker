9a2f4cf7ae43338d4cc1d57576a34df4f4624a6a
jeewonMoon <papers1210@kakao.com>
2021-07-12 11:34:19 +0900

    a

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,19 @@
 function CheckUserEmail(){
-    let email = document.getElementsByClassName('user-email').value;
-    //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
-    let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    let email = document.getElementById("userEmail").value;
+    let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
 
-    if (!regex.test(email)){
-        alert("이메일을 확인해주세요.");
-        //document.getElementsByClassName("user-email").innerHTML("올바른 이메일을 입력해주세요.");
-    }else{
-        alert("correct");   //for test
+    if (!regExp.test(email)){
+        //alert("이메일을 확인해주세요.");
+        document.getElementById("emailHelp1").innerHTML("<div 'style=color:red'>올바른 이메일을 입력해주세요.</div>");
     }
 }
 
 function CheckAdminEmail(){
-    let email = document.getElementsByClassName('admin-email').value;
-    //let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
-    let regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    let email = document.getElementById("adminEmail").value;
+    let regExp = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@([s][e][j][o][n][g]).([a][c]).([k][r])$/;
 
-    if (!regex.test(email)){
-        alert("이메일을 확인해주세요.");
-        //document.getElementsByClassName("user-email").innerHTML("올바른 이메일을 입력해주세요.");
-    }else{
-        alert("correct");   //for test
+    if (!regExp.test(email)){
+        //alert("이메일을 확인해주세요.");
+        document.getElementById("adminEmail2").innerHTML("올바른 이메일을 입력해주세요.");
     }
 }
