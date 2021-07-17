f68d4ee2c9db5583fe3a7764c8d0037b050388d5
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-07-17 11:13:36 +0900

    #cleancode
    1) 오타 수정(javascripts에서 t가 빠져 있었음)
    2) 공통되는 부분 js 파일 이용하여 모듈화

---
+++ myapp/public/javascripts/dropdown.js
@@ -0,0 +1,11 @@
+let text = '';
+let words = ['회원님', '사물함 생성', '사물함 생성', '사물함 관리', '대기열 확인', '회원정보 수정'];
+for (let i = 0; i < words.length; i++) {
+    if (i == 0) {
+        text += `<li><a class="dropdown-item" href="#"><b>${words[i]}</b></a></li>`
+    }
+    else {
+        text += `<li><a class="dropdown-item" href="#">${words[i]}</a></li>`;
+    }
+}
+document.getElementById('dropdown').innerHTML = text;
