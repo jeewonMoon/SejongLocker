b694db0b18defef6d233de74ae5c6411a34b17c8
jeewonMoon <81554184+jeewonMoon@users.noreply.github.com>
2021-07-22 19:37:18 +0900

    Cleancode (#52)
    * #page - register 가입하기 submit 버그 수정
    
    * #page - favicon, logo 적용
    
    * #page - 직급을 소속분류로 수정
    
    * #page - 미디어쿼리(로고, 세로로 나열)
    
    * #page - login email 버그 수정, checkbox 라디오형식, navbar 우측정렬
    
    * #feat 필요한 모듈 추가 및 코드 업데이트
    
    1. body-parser, mysql를 install 하였습니다.
    2. app.js에 간단히 mysql 연동 부분의 코드를 넣어봤습니다.
    
    Co-authored-by: jaeho <wogha95@naver.com>
    Co-authored-by: 박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>

--- myapp/public/javascripts/form_check.js
+++ myapp/public/javascripts/form_check.js
@@ -1,8 +1,9 @@
-let select = ['선택 안 함', '학생(회)', '학과 동아리', '교내 동아리', '교내 직원', '기타'];
+let select = ['학생(회)', '학과 동아리', '교내 동아리', '교내 직원', '기타'];
 let text = '';
 for (let i = 0; i < select.length; i++) {
     text += `<div class="form-check">
-    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault${i - 1}">
-    <label class="form-check-label" for="flexRadioDefault${i - 1}">${select[i]}</label></div>`;
+    <input class="form-check-input" type="checkbox" name="cbTeams" id="cbTeam${i - 1}" value="${i - 1}" onclick="cbTeam(this)">
+    <label class="form-check-label" for="cbTeam${i - 1}">${select[i]}</label>
+    </div>`;
 }
 document.getElementById('form-check').innerHTML = text;
