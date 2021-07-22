7c038a4c4c933ad4915e0449d4d20ebc6cc1e52e
jeewonMoon <81554184+jeewonMoon@users.noreply.github.com>
2021-07-22 20:58:30 +0900

    #cleancode 색 통일, footer 수정 (#53)
    1. 버튼, a:hover 색 통일(다른 색으로 변경O)
    2. footer의 pages 내용 변경
    -> footer(login X), footer_for_user, footer_for_admin으로 나눠서 내용 수정
    3. footer의 문의하기 수정
    -> input을 textarea로 수정, 엔터로 줄바꾸기 O, 넘치면 자동으로 줄바꾸기 O

--- myapp/public/javascripts/dropdown.js
+++ myapp/public/javascripts/dropdown.js
@@ -1,5 +1,5 @@
 let text = '';
-let words = ['회원님', '사물함 생성', '사물함 생성', '사물함 관리', '대기열 확인', '회원정보 수정'];
+let words = ['회원님', '사물함 생성', '사물함 관리', '대기열 확인', '회원정보 수정'];
 for (let i = 0; i < words.length; i++) {
     if (i == 0) {
         text += `<li><a class="dropdown-item" href="#"><b>${words[i]}</b></a></li>`
