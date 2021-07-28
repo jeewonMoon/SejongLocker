166d773a65e0b3644b84ae6c41842afa0039ef04
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-07-28 11:15:35 +0900

    #feat 세션 코드 입력
    세션을 이용하기 위한 코드를 넣었습니다.

--- myapp/routes/controllers.js
+++ myapp/routes/controllers.js
@@ -3,18 +3,24 @@ const index = (req, res) => {
 }
 
 const index_for_admin = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
     res.render('index_admin', {title:"사물함 프로젝트"});
 }
 
 const index_for_user = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
+    
     res.render('index_user', {title:"사물함 프로젝트"});
 }
 
 const locker_for_admin = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
+    
     res.render('locker_for_admin', {title:"관리자 사물함"});
 }
 
 const locker_for_user = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
     res.render('locker_for_user', {title:"사용자 사물함"});
 }
 
@@ -35,10 +41,12 @@ const register_for_user = (req, res) => {
 }
 
 const mypage_for_user = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
     res.render('mypage_user', {title:"My Page", who : "회원"});
 }
 
 const mypage_for_admin = (req, res) => {
+    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
     res.render('mypage_admin', {title:"My Page", who : "관리자"});
 }
 
