bc914403d1116dee7be24d74d7b0414a64481fc3
jeewonMoon <81554184+jeewonMoon@users.noreply.github.com>
2021-07-30 23:29:59 +0900

    Cleancode (#60)
    * #cleancode 로그인 학번으로 수정
    
    1. 학번으로 로그인하게 바꾸면서 기존 login.js를 email_check.js로 파일 이름 변경
    2. 새로운 login.js
    -> 학번이나 비밀번호를 입력하지 않았을 경우 alert
    
    * #feat 로그인 로그아웃 구현
    
    세부 오류들은 추후에 바로 수정하겠습니다.
    
    * #feat 로그인 로그아웃 구현
    
    1) 로그인, 로그아웃 구현
    2) 기존 템플릿 문법 조금 수정(<%= 사용 삭제했습니다.)
    
    * #feat 로그인, 로그아웃 구현
    
    1) 로그인 로그아웃을 process라는 객체 안에서 관리하도록 하였습니다.
    2) 마이페이지 등도 세션에 따라 다른 화면이 나오도록 설정하였습니다.
    
    * #cleancode 컨트롤러 변수명 변경
    
    컨트롤러에 사용되는 객체 변수명을 카멜 표기법으로 변경하였습니다. 혹여 잘 실행되지 않을 수도 있으므로 기존 코드는 지우지 않고 남겨두었습니다.
    
    * #feat 사용자 회원가입 기능 추가(진행중)
    
    1) 사용자 회원가입 구현하였습니다.(관리자는 아직입니다.)
    2) 회원가입 과정을 따로 처리하는 registerProcess를 만들었습니다.
    
    Co-authored-by: 박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>

--- myapp/routes/controllers.js
+++ myapp/routes/controllers.js
@@ -1,87 +1,175 @@
-const index = (req, res) => {
-    res.render('index', {title : "사물함 프로젝트"});
-}
+const con = require('../db/config');
 
-const index_for_admin = (req, res) => {
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    res.render('index_admin', {title:"사물함 프로젝트"});
-}
-
-const index_for_user = (req, res) => {
-    console.log(req.body);
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    
-    res.render('index_user', {title:"사물함 프로젝트"});
-}
-
-const locker_for_admin = (req, res) => {
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    
-    res.render('locker_for_admin', {title:"관리자 사물함"});
-}
-
-const locker_for_user = (req, res) => {
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    res.render('locker_for_user', {title:"사용자 사물함"});
-}
-
-const login = (req, res) => {
-    res.render('login', {title:"로그인", user : "사용자", admin : "관리자"});
-}
-
-const login = (req, res) => {
-    if(req.session.user){
-        console.log('로그아웃 처리');
-        req.session.destroy(
-            function (err){
-                if(err){
-                    console.log('세션 삭제시 에러');
-                    return;
+const get = {
+    index : (req, res) => {
+        console.log(req.session);
+        if(req.session.user){
+            console.log("사용자로 로그인");
+            res.redirect('/index_user');
+        }
+        else if(req.session.admin){
+            console.log("관리자로 로그인");
+            res.redirect('/index_admin');
+        }
+        else
+            res.render('index');
+    },
+    indexForAdmin : (req, res) => {
+        console.log(req.session);
+        if(req.session.admin)
+            res.render('index_admin')
+        else{
+            res.redirect('/');
+        }
+    },
+    indexForUser : (req, res) => {
+        console.log(req.session);
+        if(req.session.user){
+            console.log("사용자로 로그인");
+            res.render('index_user');
+        }
+        else
+            res.redirect('/');
+    },
+    //redirect -> url로 이동시킴, render -> 템플릿 출력
+    lockerForAdmin : (req, res) => {
+        if(req.session.admin)
+            res.render('locker_for_admin');
+        else
+            res.redirect('/');
+    },
+    lockerForUser : (req, res) => {
+        if(req.session.user)
+            res.render('locker_for_user');
+        else
+            res.redirect('/');
+    },
+    registerChoice : (req, res) => {
+        res.render('register_choice');
+    },
+    registerForAdmin : (req, res) => {
+        res.render('register_for_admin');
+    },
+    registerForUser : (req, res) => {
+        res.render('register_for_user');
+    },
+    mypageForUser : (req, res) => {
+        //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
+        console.log(req.session);
+        if(req.session.user)
+            res.render('mypage_for_user');
+        else
+            res.redirect('/');
+    },
+    mypageForAdmin : (req, res) => {
+        //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
+        console.log(req.session);
+        if(req.session.admin)
+            res.render('mypage_for_admin');
+        else
+            res.redirect('/');
+    },
+    login : (req, res) => {
+        //데이터베이스 확인 후, 작업
+        res.render('login');
+    },
+    logout : (req, res) => {
+        if(req.session.user){
+            console.log('로그아웃 처리');
+            req.session.destroy(
+                function (err){
+                    if(err){
+                        console.log('세션 삭제시 에러');
+                        return;
+                    }
+                    console.log('세션 삭제 성공');
+                    res.redirect('/');
+                    //에러 있을겁니다. 추후 수정할게요.
                 }
-                console.log('세션 삭제 성공');
-                res.redirect('/');
-                //에러 있을겁니다. 추후 수정할게요.
+            );
+        }
+
+        else if(req.session.admin){
+            console.log('로그아웃 처리');
+            req.session.destroy(
+                function (err){
+                    if(err){
+                        console.log('세션 삭제시 에러');
+                        return;
+                    }
+                    console.log('세션 삭제 성공');
+                    res.redirect('/');
+                    //에러 있을겁니다. 추후 수정할게요.
+                }
+            );
+        }
+
+        else{
+            console.log("로그인 안 되어있음");
+            res.redirect('/');
+        }
+    }
+};
+
+const process = {
+    registerProcessForUser : (req, res) => {
+        let id = req.body.userid;
+        let name = req.body.name;
+        let email = req.body.email;
+        let phone = req.body.phonenum;
+        let password = req.body.password;
+        let team = req.body.team;
+        console.log(id, name, email, phone, password, team);
+        const sql = 'INSERT INTO user (userid, name, email, phonenum, password, team) values (?, ?, ?, ?, ?, ?)';
+        const params = [id, name, email, phone, password, team];
+        con.query(sql, params, function(err, rows, fields){
+            if(err)
+                throw err;
+            else{
+                console.log(rows);
+                res.send('회원가입이 완료되었습니다.');
             }
-        );
+        })
+        //res.redirect('/register_choice');
+    },
+    registerProcessForAdmin : (req, res) => {
+
+    },
+    loginProcessForUser : (req, res) => {
+        //세션이 없는 경우 세션 발급
+        let id = req.body.userId;
+        let password = req.body.userPassword;
+        console.log(id, password);
+        if(req.session.user){
+            console.log("이미 로그인 되어 있음");
+        }
+        else{
+            req.session.user = {
+                id : id,
+            }
+            console.log("로그인 처음 됨");
+        }
+        res.redirect('/index_user');
+    },
+    loginProcessForAdmin : (req, res) => {
+        //세션이 없는 경우 세션 발급
+        let id = req.body.adminId;
+        let password = req.body.adminPassword;
+        console.log(id, password);
+        if(req.session.admin){
+            console.log("이미 로그인 되어 있음");
+        }
+        else{
+            req.session.admin = {
+                id : id,
+            }
+            console.log("로그인 처음 됨");
+        }
+        res.redirect('/index_admin');
     }
-    else{
-        console.log("로그인 안 되어있음");
-        res.redirect('/');
-    }
-}
-
-const register_choice = (req, res) => {
-    res.render('register_choice', {title:"회원가입"});
-}
-
-const register_for_admin = (req, res) => {
-    res.render('register_for_admin', {title:"관리자 회원가입", who : "관리자"});
-}
-
-const register_for_user = (req, res) => {
-    res.render('register_for_user', {title:"사용자 회원가입", who : "사용자"});
-}
-
-const mypage_for_user = (req, res) => {
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    res.render('mypage_user', {title:"My Page", who : "회원"});
-}
-
-const mypage_for_admin = (req, res) => {
-    //세션이 없는 경우 인덱스 혹은 로그인 페이지로 돌려보내게 할 것입니다.
-    res.render('mypage_admin', {title:"My Page", who : "관리자"});
 }
 
 module.exports = {
-    index,
-    index_for_admin,
-    index_for_user,
-    locker_for_admin,
-    locker_for_user,
-    login,
-    register_choice,
-    register_for_admin,
-    register_for_user,
-    mypage_for_user,
-    mypage_for_admin   
+    get,
+    process
 }
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,26 +4,49 @@ const nodemailer = require('nodemailer');
 const controller = require('./controllers');
 /* GET home page. */
 
-router.get('/', controller.index);
-router.post('/', controller.index);
+router.get('/', controller.get.index);
 
-router.get('/index_admin', controller.index_for_admin);
-router.post('/index_admin', controller.index_for_admin);
+/*
+router.get('/index_admin', controller.get.index_for_admin);
+router.get('/index_user', controller.get.index_for_user);
+*/
+router.get('/index_admin', controller.get.indexForAdmin);
+router.get('/index_user', controller.get.indexForUser);
 
-router.get('/index_user', controller.index_for_user);
-router.post('/index_user', controller.index_for_user);
+/*
+router.get('/locker_for_admin', controller.get.locker_for_admin);
+router.get('/locker_for_user', controller.get.locker_for_user);
+*/
 
-router.get('/locker_for_admin', controller.locker_for_admin);
-router.get('/locker_for_user', controller.locker_for_user);
+router.get('/locker_for_admin', controller.get.lockerForAdmin);
+router.get('/locker_for_user', controller.get.lockerForUser);
 
-router.get('/login', controller.login);
+router.get('/login', controller.get.login);
+router.post('/loginProcessForUser', controller.process.loginProcessForUser);
+router.post('/loginProcessForAdmin', controller.process.loginProcessForAdmin);
 
-router.get('/register_choice', controller.register_choice);
-router.get('/register_for_admin', controller.register_for_admin);
-router.get('/register_for_user', controller.register_for_user);
+router.post('/registerProcessForUser', controller.process.registerProcessForUser);
+router.post('/registerProcessForAdmin', controller.process.registerProcessForAdmin);
 
-router.get('/mypage_for_admin', controller.mypage_for_admin);
-router.get('/mypage_for_user', controller.mypage_for_user);
+router.get('/logout', controller.get.logout);
+
+/*
+router.get('/register_choice', controller.get.register_choice);
+router.get('/register_for_admin', controller.get.register_for_admin);
+router.get('/register_for_user', controller.get.register_for_user);
+*/
+
+router.get('/register_choice', controller.get.registerChoice);
+router.get('/register_for_admin', controller.get.registerForAdmin);
+router.get('/register_for_user', controller.get.registerForUser);
+
+/*
+router.get('/mypage_for_admin', controller.get.mypage_for_admin);
+router.get('/mypage_for_user', controller.get.mypage_for_user);
+*/
+
+router.get('/mypage_for_admin', controller.get.mypageForAdmin);
+router.get('/mypage_for_user', controller.get.mypageForUser);
 
 /* HTML 사용시 이용
 router.get('/', function(req, res, next) {
