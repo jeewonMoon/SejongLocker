ef54bf6115c09d78eb0c859b88c11111b19ffb8e
박현우(Park Hyun Woo) <46596758+gusdn3477@users.noreply.github.com>
2021-08-01 18:04:33 +0900

    #feat 가입 문구 추가
    가입된 경우 로그인창에 문구가 출력되도록 하였습니다.

--- myapp/routes/controllers.js
+++ myapp/routes/controllers.js
@@ -98,7 +98,7 @@ const get = {
     },
     login : (req, res) => {
         //데이터베이스 확인 후, 작업
-        res.render('login');
+        res.render('login', {message:""});
     },
     logout : (req, res) => {
         if(req.session.user){
@@ -156,7 +156,7 @@ const process = {
             else{
                 console.log(rows);
                 // res.send('회원가입이 완료되었습니다.');
-                res.render('login');
+                res.render('login', {message:"사용자 회원가입이 완료되었습니다."});
             }
         })
         //res.redirect('/register_choice');
@@ -178,7 +178,7 @@ const process = {
             else{
                 console.log(rows);
                 // res.send('회원가입이 완료되었습니다.');
-                res.render('login');
+                res.render('login', {message:"관리자 회원가입이 완료되었습니다."});
             }
         })
     },
