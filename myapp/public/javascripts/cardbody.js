6a2c8297fca5b4296bfd9a5055d6be7a55ba8831
jaeho <wogha95@naver.com>
2021-08-12 00:55:57 +0900

    #page - 회원가입 사용자 관리자 합치기, 프로필 변경 버튼 삭제

--- myapp/public/javascripts/cardbody.js
+++
@@ -1,7 +0,0 @@
-let text = '';
-let words_key = ['이름', '이메일', '학번'];
-let words_values = ['OYR', 'OYR@sejong.ac.kr', '210717'];
-for(let i=0;i<words_key.length;i++){
-    text += `<h5 class="card-title">${words_key[i]}</h5><p class="card-text">${words_values[i]}</p>`;
-}
-document.getElementById('card-body').innerHTML = text;
--- myapp/public/javascripts/form_check.js
+++
@@ -1,9 +0,0 @@
-let select = ['학생(회)', '학과 동아리', '교내 동아리', '교내 직원', '기타'];
-let text = '';
-for (let i = 0; i < select.length; i++) {
-    text += `<div class="form-check">
-    <input class="form-check-input" type="checkbox" name="cbTeams" id="cbTeam${i}" value="${i}" onclick="cbTeam(this)">
-    <label class="form-check-label" for="cbTeam${i}">${select[i]}</label>
-    </div>`;
-}
-document.getElementById('form-check').innerHTML = text;
