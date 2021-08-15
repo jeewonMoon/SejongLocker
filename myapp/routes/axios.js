8bde8c2d68c560f411602d2015d68e18d229c30e
jeewonMoon <papers1210@kakao.com>
2021-08-15 20:44:47 +0900

    #feat 회원가입 학번 중복 체크
    1. 학번 중복 검사
    -> 이미 있는 경우: alert('이미 있는 id');
    -> 새로운 id: alert('사용 가능합니다.');
    2. 비동기식으로 처리하면서 mysql async/await을 사용하기 위해 mysql2 설치(npm install mysql2)
    -> db/config.js 수정

--- myapp/routes/axios.js
+++ myapp/routes/axios.js
@@ -1,2 +1,37 @@
 const axios = require('axios');
+const { con } = require('../db/config');
 
+// axios.get('/idCheck', (req, res) => {
+//     let id = req.body.id;
+//     if(req.body.radioCheck == "사용자"){
+//         console.log('사용자 id 중복확인');
+//         const sql = `SELECT userid FROM USER WHERE userid = ?`;
+//         const params = [id];
+//         con.query(sql, params, function(err, rows, fields){
+//             if(err)
+//                 throw err;
+//             else{
+//                 console.log(rows);
+//                 if(rows.length > 0){
+//                     alert('이미 존재하는 학번입니다.');
+//                 }
+//             }
+//         })
+//     }else if(req.body.radioCheck == "관리자"){
+//         console.log('관리자 id 중복확인');
+//         const sql = `SELECT adminid FROM ADMIN WHERE adminid = ?`;
+//         const params = [id];
+//         con.query(sql, params, function(err, rows, fields){
+//             if(err)
+//                 throw err;
+//             else{
+//                 console.log(rows);
+//                 if(rows.length > 0){
+//                     alert('이미 존재하는 학번입니다.');
+//                 }
+//             }
+//         })
+//     }
+//     else
+//         res.redirect('/register');
+// })
