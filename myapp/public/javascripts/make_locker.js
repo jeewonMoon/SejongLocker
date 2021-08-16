b1c10083c429039c4629fc41da3f80475a064bef
jaeho <wogha95@naver.com>
2021-08-17 00:41:58 +0900

    #bugfix - 로그인 화면 개선

--- myapp/public/javascripts/make_locker.js
+++ myapp/public/javascripts/make_locker.js
@@ -26,15 +26,6 @@ function blockLocker(id) {
         num.className = "table-primary";
         exceptuse.value = id;
     }
-    
-   /* id를 추후에도 사용할 것이라 판단되어, this를 사용한 방법은 잠시 보류하겠습니다.
-    if (id.className == "table-primary") {
-        id.className = "table-secondary";
-    }
-    else {
-        id.className = "table-primary";
-    }
-    */
 }
 
 
