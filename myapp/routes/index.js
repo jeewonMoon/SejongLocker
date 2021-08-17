835c98647ff7f4c88560cb64e176cafbb763c1c7
jaeho <wogha95@naver.com>
2021-08-17 12:01:06 +0900

    #feat - nodemailer 구현

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
 const controller = require('./controllers');
+
+// nodemailer를 위한 모듈
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+let appDir = path.dirname(__dirname);
+
 /* GET home page. */
 
 router.get('/', controller.get.index);
@@ -40,5 +46,52 @@ router.post('/makelocker', controller.process.makeLocker);
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });*/
+
+router.get('/register/authEmail', async(req, res) => {
+  let authEmail = req.query.authEmail;
+  let authNum = Math.random().toString().substr(2,6);
+  console.log("## 이메일 넘겨짐" + authEmail);
+  /*
+  // console.log(appDir);
+  authEmail += "@sju.ac.kr"
+  let emailTemplete;
+  ejs.renderFile(appDir+'/routes/authMail.ejs', {authCode : authNum}, function (err, data) {
+    if(err){console.log(err)}
+    emailTemplete = data;
+  });
+
+  let transporter = nodemailer.createTransport({
+    // 여기를 mailtrap계정의 내용으로 변경하시면 이메일 전송 여부 확인 가능
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      secure: false,
+      auth: {
+          user: "33ac9092d6325e",
+          pass: "faadb9b103f543",
+      },
+  });
+
+  let mailOptions = {
+      from: `noreply@SejongLocker.com`,
+      to: authEmail,
+      // to: "981e84eae5-96c52d@inbox.mailtrap.io",
+      subject: '회원가입을 위한 인증번호를 입력해주세요.',
+      html: emailTemplete,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+          console.log(error);
+      }
+      console.log("Finish sending email : " + info.response);
+      transporter.close()
+  });
+  */
+  res.json({
+    authNum : authNum,
+    authEmail : authEmail
+  });
+  
+});
 
 module.exports = router;
